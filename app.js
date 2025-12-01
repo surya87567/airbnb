dd24ed7e83e83b7168c53c18921a95119cc4ea66
Surya <singhsuryapratap058@gmail.com>
2025-12-01 12:02:45 +0530

    authentication using cookies and session

--- app.js
+++ app.js
@@ -1,38 +1,64 @@
 // Core Module
 const path = require('path');
-const mongoose=require('mongoose');
+const mongoose = require('mongoose');
 
 // External Module
 const express = require('express');
+const cookieParser = require('cookie-parser');
 
-//Local Module
-const storeRouter = require("./routes/storeRouter")
-const hostRouter = require("./routes/hostRouter")
+// Local Module
+const storeRouter = require("./routes/storeRouter");
+const hostRouter = require("./routes/hostRouter");
+const authRouter = require("./routes/authRouter");
 const rootDir = require("./utils/pathUtil");
 const errorsController = require("./controllers/errors");
-//const {mongoConnect} = require('./utils/databaseUtil');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-app.use(express.urlencoded());
+// Body parser
+app.use(express.urlencoded({ extended: false }));
+
+// Cookie parser
+app.use(cookieParser());
+
+// Static files (should come BEFORE protected routes)
+app.use(express.static(path.join(rootDir, 'public')));
+
+// Middleware to check login status
+app.use((req, res, next) => {
+  req.isLoggedIn = req.cookies.isLoggedIn === "true";
+  next();
+});
+
+// Routers
+app.use(authRouter);
 app.use(storeRouter);
+
+// Protect host routes
+app.use("/host", (req, res, next) => {
+  if (req.isLoggedIn) next();
+  else res.redirect("/login");
+});
+
 app.use("/host", hostRouter);
 
-app.use(express.static(path.join(rootDir, 'public')))
-
+// Error page
 app.use(errorsController.pageNotFound);
 
 const PORT = 3000;
 
 const MONGO_URL = "mongodb+srv://root:root@cluster0.8y68az1.mongodb.net/airbnb?retryWrites=true&w=majority&appName=Cluster0";
-mongoose.connect(MONGO_URL).then(()=>{
-  console.log('Connected to mongo');
-  app.listen(PORT, () => {
-    console.log(`Server running on address http://localhost:${PORT}`);
+
+mongoose.connect(MONGO_URL)
+  .then(() => {
+    console.log('Connected to mongo');
+    app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
+  })
+  .catch(err => {
+    console.log('Error while connecting to mongo:', err);
   });
-}).catch(err=>{
-  console.log('Error while connecting to mongo:',err);
-})
--- models/home.js
+++ models/home.js
@@ -1,34 +1,32 @@
-const { ObjectId } = require('mongodb');
-const mongoose=require('mongoose');
+const mongoose = require('mongoose');
 const favourite = require('./favourite');
 
-const homeSchema=mongoose.Schema({
-  houseName:{
-    type:String,
-    required:true
+const homeSchema = mongoose.Schema({
+  houseName: {
+    type: String,
+    required: true
   },
-  price:{
-    type:Number,
-    required:true
+  price: {
+    type: Number,
+    required: true
   },
-  location:{
-    type:String,
-    required:true
+  location: {
+    type: String,
+    required: true
   },
-  rating:{
-    type:Number,
-    required:true
+  rating: {
+    type: Number,
+    required: true
   },
-  photoUrl:String,
-  description:String,
-
+  photoUrl: String,
+  description: String,
 });
 
-homeSchema.pre('findOneAndDelete',async function(next){
-  console.log("came to pre hook while deleting a home");
-  const homeId=this.getquery()._id;
-  await favourite.deleteMany({houseId:homeId});
+homeSchema.pre('findOneAndDelete', async function(next) {
+  console.log('Came to pre hook while deleting a home');
+  const homeId = this.getQuery()._id;
+  await favourite.deleteMany({houseId: homeId});
   next();
 });
 
-module.exports=mongoose.model('Home',homeSchema);
+module.exports = mongoose.model('Home', homeSchema);
